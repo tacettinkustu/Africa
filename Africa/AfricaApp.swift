4f58a35d9293124c3c208408ac78a5967282e982
Tacettin Kustu <tacettinkustu@gmail.com>
2023-06-18 12:31:03 +0300

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Tacettin Küstü on 18.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
