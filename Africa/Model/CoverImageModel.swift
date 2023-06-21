149cc3f3237cfc8f18f8bd811ce302af887329bd
Tacettin Kustu <tacettinkustu@gmail.com>
2023-06-21 22:41:41 +0300

    Create a new model for coverImage data

---
+++ Africa/Model/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImageModel.swift
+//  Africa
+//
+//  Created by Tacettin Küstü on 21.06.2023.
+//
+
+import SwiftUI
+
+struct CoverImage: Codable, Identifiable {
+  let id: Int
+  let name: String
+}
