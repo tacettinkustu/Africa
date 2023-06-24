50bc28064f9fe0b2d40ad0f26a3051c3dc018efd
Tacettin Kustu <tacettinkustu@gmail.com>
2023-06-24 14:25:10 +0300

    Create InsetFactView component and implement it

--- Africa/View/AnimalDetailView.swift
+++ Africa/View/AnimalDetailView.swift
@@ -8,59 +8,65 @@
 import SwiftUI
 
 struct AnimalDetailView: View {
-  
-  let animal: Animal
-
-  var body: some View {
-    ScrollView(.vertical, showsIndicators: false) {
-      VStack(alignment: .center, spacing: 20) {
-        // HERO IMAGE
-        Image(animal.image)
-          .resizable()
-          .scaledToFit()
-        
-        // TITLE
-        Text(animal.name.uppercased())
-          .font(.largeTitle)
-          .fontWeight(.heavy)
-          .multilineTextAlignment(.center)
-          .padding(.vertical, 8)
-          .foregroundColor(.primary)
-          .background(
-            Color.accentColor
-              .frame(height: 6)
-              .offset(y: 24)
-          )
-          
-        // HEADLINE
-        Text(animal.headline)
-          .font(.headline)
-          .multilineTextAlignment(.leading)
-          .foregroundColor(.accentColor)
-          .padding(.horizontal)
-        
-        // GALLERY
-          Group {
-            HeadingView(headingImage: "photo.on.rectangle.angled", headingText: "Wilderness in Pictures")
-              InsetGalleryView(animal: animal)
-          }
-          .padding(.horizontal)
-        // FACTS
-        //
-        
-      } //: VSTACK
-      .navigationBarTitle("Learn about \(animal.name)", displayMode: .inline)
-    } //: SCROLL
-  }
+    
+    let animal: Animal
+    
+    var body: some View {
+        ScrollView(.vertical, showsIndicators: false) {
+            VStack(alignment: .center, spacing: 20) {
+                // HERO IMAGE
+                Image(animal.image)
+                    .resizable()
+                    .scaledToFit()
+                
+                // TITLE
+                Text(animal.name.uppercased())
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .multilineTextAlignment(.center)
+                    .padding(.vertical, 8)
+                    .foregroundColor(.primary)
+                    .background(
+                        Color.accentColor
+                            .frame(height: 6)
+                            .offset(y: 24)
+                    )
+                
+                // HEADLINE
+                Text(animal.headline)
+                    .font(.headline)
+                    .multilineTextAlignment(.leading)
+                    .foregroundColor(.accentColor)
+                    .padding(.horizontal)
+                
+                // GALLERY
+                Group {
+                    HeadingView(headingImage: "photo.on.rectangle.angled", headingText: "Wilderness in Pictures")
+                    InsetGalleryView(animal: animal)
+                }
+                .padding(.horizontal)
+                
+                // FACTS
+                Group {
+                    HeadingView(headingImage: "questionmark.circle", headingText: "Did you know?")
+                    
+                    InsetFactView(animal: animal)
+                }
+                .padding(.horizontal)
+                
+            } //: VSTACK
+            .navigationBarTitle("Learn about \(animal.name)", displayMode: .inline)
+        } //: SCROLL
+    }
 }
 
 struct AnimalDetailView_Previews: PreviewProvider {
-  static let animals: [Animal] = Bundle.main.decode("animals.json")
-  
-  static var previews: some View {
-    NavigationView {
-      AnimalDetailView(animal: animals[0])
+    static let animals: [Animal] = Bundle.main.decode("animals.json")
+    
+    static var previews: some View {
+        NavigationView {
+            AnimalDetailView(animal: animals[0])
+        }
+        .previewDevice("iPhone 14")
     }
-    .previewDevice("iPhone 14")
-  }
 }
