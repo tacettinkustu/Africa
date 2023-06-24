ddd56d4ab28f75a0f56da140a0fdb39f90511236
Tacettin Kustu <tacettinkustu@gmail.com>
2023-06-24 13:59:37 +0300

    Create headingView component

---
+++ Africa/View/HeadingView.swift
@@ -0,0 +1,36 @@
+//
+//  HeadingView.swift
+//  Africa
+//
+//  Created by Tacettin Küstü on 24.06.2023.
+//
+
+import SwiftUI
+
+struct HeadingView: View {
+    
+  var headingImage: String
+  var headingText: String
+  
+  var body: some View {
+    HStack {
+      Image(systemName: headingImage)
+        .foregroundColor(.accentColor)
+        .imageScale(.large)
+      
+      Text(headingText)
+        .font(.title3)
+        .fontWeight(.bold)
+    }
+    .padding(.vertical)
+  }
+}
+
+struct HeadingView_Previews: PreviewProvider {
+  static var previews: some View {
+    HeadingView(headingImage: "photo.on.rectangle.angled", headingText: "Wilderness in Pictures")
+      .previewLayout(.sizeThatFits)
+      .padding()
+  }
+}
+
