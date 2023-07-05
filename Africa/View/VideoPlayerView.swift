552b41aab6812a6addeafaffa4a195f529a6a15a
Tacettin Kustu <tacettinkustu@gmail.com>
2023-07-05 21:36:28 +0300

    Create a new video view

---
+++ Africa/View/VideoPlayerView.swift
@@ -0,0 +1,43 @@
+//
+//  VideoPlayerView.swift
+//  Africa
+//
+//  Created by Tacettin Küstü on 5.07.2023.
+//
+
+import SwiftUI
+import AVKit
+
+struct VideoPlayerView: View {
+  
+  var videoSelected: String
+  var videoTitle: String
+
+  var body: some View {
+    VStack {
+      VideoPlayer(player: playVideo(fileName: videoSelected, fileFormat: "mp4")) {
+         // Text
+      }
+      .overlay(
+        Image("logo")
+          .resizable()
+          .scaledToFit()
+          .frame(width: 32, height: 32)
+          .padding(.top, 6)
+          .padding(.horizontal, 8)
+        , alignment: .topLeading
+      )
+    } //: VSTACK
+    .accentColor(.accentColor)
+    .navigationBarTitle(videoTitle, displayMode: .inline)
+  }
+}
+
+struct VideoPlayerView_Previews: PreviewProvider {
+  static var previews: some View {
+    NavigationView {
+      VideoPlayerView(videoSelected: "lion", videoTitle: "Lion")
+    }
+  }
+}
+
